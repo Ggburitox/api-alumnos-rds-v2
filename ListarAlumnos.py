6a3f9c926e0b75e5d654d9181a10a5f00fe5d988
Thiago Rivera <165909519+Ggburitox@users.noreply.github.com>
2025-07-19 00:07:38 -0500

    Add files via upload

---
+++ ListarAlumnos.py
@@ -0,0 +1,46 @@
+import boto3
+import pymysql
+import os
+import json
+
+def lambda_handler(event, context):
+    secret_name = os.environ['SECRET_NAME']
+    region_name = os.environ.get('AWS_REGION', 'us-east-1')
+
+    try:
+
+        client = boto3.client('secretsmanager', region_name=region_name)
+        response = client.get_secret_value(SecretId=secret_name)
+        secret = json.loads(response['SecretString'])
+
+        host = secret['host']
+        user = secret['username']
+        password = secret['password']
+        database = secret['dbname']
+
+        connection = pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            db=database,
+            connect_timeout=5
+        )
+
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM alumnos;")
+            results = cursor.fetchall()
+
+        return {
+            "statusCode": 200,
+            "body": results
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "error": str(e)
+        }
+
+    finally:
+        if 'connection' in locals():
+            connection.close()
